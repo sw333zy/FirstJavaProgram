6fade44ba7da01d9f6a304c7e24801564394e482
sw333zy <sw333zy@gmail.com>
2019-01-24 09:31:05 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World!");
+	System.out.println("Today is January 24, 2019. I am alive!");
+	System.out.println("My name is Josiah.");
+    }
+}
